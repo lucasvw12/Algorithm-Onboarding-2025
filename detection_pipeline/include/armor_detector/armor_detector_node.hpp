3b94a87c8a78105f877f7b6ded0c3d4f546e040c
lucasvw12 <lucas.vanwinckel@gmail.com>
2026-09-11 15:34:45 -0400

    Make armor detector a ROS node

--- detection_pipeline/include/armor_detector/armor_detector_node.hpp
+++ detection_pipeline/include/armor_detector/armor_detector_node.hpp
@@ -5,7 +5,7 @@
  *  class.
  *
  *  Since this file implements a ROS node like camera_publisher_node does, you may want to refer to that file to get
- *  ideas for things you need to consider while writing your code.
+ *  ideas about things you need to consider while writing your code.
  */
 
 #include <iostream>
@@ -29,34 +29,51 @@
 #define ARMOR_HEIGHT_RATIO_LIMIT 1.5
 #define ARMOR_ASPECT_RATIO_LIMIT 2.5
 
-// Define the Red HSV color range for segmentation 
-cv::Scalar lowerHSV(0, 120, 70);   // Lower bound of HSV
-cv::Scalar upperHSV(10, 255, 255); // Upper bound of HSV
-cv::Scalar lowerHSV2(170, 120, 70);   // Lower bound of HSV
-cv::Scalar upperHSV2(179, 255, 255); // Upper bound of HSV
+// Define the Red HSV color range for segmentation
+cv::Scalar lowerHSV(0, 120, 70);
+cv::Scalar upperHSV(10, 255, 255);
+cv::Scalar lowerHSV2(170, 120, 70);
+cv::Scalar upperHSV2(179, 255, 255);
 
-class ArmorDetectorNode : /* TODO: Can you find the ROS node base class? We want our class to inherit from it. */ {
+class ArmorDetectorNode : public rclcpp::Node {
     public:
         ArmorDetectorNode();
+
     private:
-        // This is a subscription. Since our node contains it, we consider our node to be a subscriber.
-        // Don't worry too much about the types, but basically we define this subscription as one that fetches images.
+        // Subscription to the camera image topic
         rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr image_sub_;
 
-        // We want to track the frame number for debugging purposes
+        // Track frame number
         int frame_count;
-        
-        // This callback will be how this node actually reads images published to the topic.
-        // You will learn more about that in armor_detector_node.cpp
+
+        // Callback for incoming images
         void image_callback(const sensor_msgs::msg::Image::SharedPtr msg);
 
-        // This method will display processed frames. How else are we going to judge what your code does?
+        // Display processed frames
         void show_frame(cv::Mat &frame);
 
         // Armor detection methods
-        std::vector<cv::RotatedRect> search(cv::Mat &frame, cv::Scalar lowerHSV, cv::Scalar upperHSV, cv::Scalar lowerHSV2, cv::Scalar upperHSV2);
+        std::vector<cv::RotatedRect> search(
+            cv::Mat &frame,
+            cv::Scalar lowerHSV,
+            cv::Scalar upperHSV,
+            cv::Scalar lowerHSV2,
+            cv::Scalar upperHSV2
+        );
+
         bool is_light_bar(cv::RotatedRect &rect);
-        bool is_armor(cv::RotatedRect &left_rect, cv::RotatedRect &right_rect);
-        void draw_rotated_rect(cv::Mat &frame, cv::RotatedRect &rect);
-        std::vector<cv::Point2f> rect_to_point(cv::RotatedRect &rect);
+
+        bool is_armor(
+            cv::RotatedRect &left_rect,
+            cv::RotatedRect &right_rect
+        );
+
+        void draw_rotated_rect(
+            cv::Mat &frame,
+            cv::RotatedRect &rect
+        );
+
+        std::vector<cv::Point2f> rect_to_point(
+            cv::RotatedRect &rect
+        );
 };
